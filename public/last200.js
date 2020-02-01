31ff6980e4302fed3460dc87dae5e66476f3d3f0
alexivar <alexivar65@gmail.com>
2020-02-01 00:01:15 +0000

    changing to flight planning site

--- public/last200.js
+++ public/last200.js
@@ -1,5 +1,97 @@
 
   //your code here
+  document.getElementById('empty_leverarm').innerHTML = ""
+  document.getElementById('empty_mass').innerHTML = ""
+  document.getElementById('empty_moment').innerHTML = ""
+  document.getElementById('frontseats_mass').innerHTML = ""
+  document.getElementById('frontseats_moment').innerHTML = ""
+  document.getElementById('rearseats_mass').innerHTML = ""
+  document.getElementById('rearseats_moment').innerHTML = ""
+  document.getElementById('stdbaggage_mass').innerHTML = ""
+  document.getElementById('stdbaggage_moment').innerHTML = ""
+
+  document.getElementById('baggagetube_mass').innerHTML = "-"
+  document.getElementById('baggagetube_moment').innerHTML = "-"
+  document.getElementById('shortbaggage_mass').innerHTML = "-"
+  document.getElementById('shortbaggage_moment').innerHTML = "-"
+  document.getElementById('fwdbaggage_mass').innerHTML = "-"
+  document.getElementById('fwdbaggage_moment').innerHTML = "-"
+  document.getElementById('aftbaggage_mass').innerHTML = "-"
+  document.getElementById('aftbaggage_moment').innerHTML = "-"
+
+  document.getElementById('zfm_leverarm').innerHTML = ""
+  document.getElementById('zfm_mass').innerHTML = ""
+  document.getElementById('zfm_moment').innerHTML = ""
+  document.getElementById('fuel_mass').innerHTML = ""
+  document.getElementById('fuel_moment').innerHTML = ""
+  document.getElementById('tom_leverarm').innerHTML = ""
+  document.getElementById('tom_mass').innerHTML = ""
+  document.getElementById('tom_moment').innerHTML = ""
+  document.getElementById('fuelburn_mass').innerHTML = ""
+  document.getElementById('fuelburn_moment').innerHTML = ""
+  document.getElementById('lm_leverarm').innerHTML = ""
+  document.getElementById('lm_mass').innerHTML = ""
+  document.getElementById('lm_moment').innerHTML = ""
+
+function myFunction(){
+  var mass = document.getElementById("mass").value;
+  var moment = document.getElementById("moment").value;
+  var frontSeat1 = document.getElementById("frontSeat1").value;
+  var frontSeat2 = document.getElementById("frontSeat2").value;
+  var backSeats = document.getElementById("backSeats").value;
+  var baggage = document.getElementById("baggage").value;
+  var fuel = document.getElementById("fuel").value;
+  var fuelburn = document.getElementById("fuelburn").value;
+  var emptyleverarm = roundToTwo(moment/mass);
+  var frontseatmass = +frontSeat1 + +frontSeat2;
+  console.log(frontseatmass);
+  var fronseatmoment = roundToTwo(frontseatmass * 2.3);
+  var backseatmoment = roundToTwo(3.25 * backSeats);
+  var baggagemoment = roundToTwo(3.65 * baggage);
+  var zfmMass = +mass + +frontseatmass + +backSeats + +baggage;
+  var zfmMoment = +moment + +fronseatmoment + +backseatmoment + +baggagemoment;
+  var zfmleverarm = roundToTwo(zfmMoment / zfmMass);
+  var fuelmass = roundToTwo(fuel * 3.03);
+  var fuelmoment = roundToTwo(fuelmass * 2.63);
+  var tomMass = +zfmMass + +fuelmass;
+  var tomMoment = +zfmMoment + +fuelmoment;
+  var tomLeverarm = roundToTwo(tomMoment / tomMass);
+  var fuelburnmass = roundToTwo(3.03 * fuelburn);
+  var fuelburnmoment = roundToTwo(fuelburnmass * 2.63);
+  var lmMass = roundToTwo(tomMass - fuelburnmass);
+  var lmMoment = roundToTwo(tomMoment - fuelburnmoment);
+  var lmLeverarm = roundToTwo(lmMoment / lmMass);
+
+  document.getElementById('empty_leverarm').innerHTML = emptyleverarm;
+  document.getElementById('empty_mass').innerHTML = mass;
+  document.getElementById('empty_moment').innerHTML = moment;
+
+  document.getElementById('frontseats_mass').innerHTML = frontseatmass;
+  document.getElementById('frontseats_moment').innerHTML = fronseatmoment;
+  document.getElementById('rearseats_mass').innerHTML = ((backSeats == '0' || backSeats == '') ? '-' : backSeats);
+  document.getElementById('rearseats_moment').innerHTML = backseatmoment;
+  document.getElementById('stdbaggage_mass').innerHTML = baggage;
+  document.getElementById('stdbaggage_moment').innerHTML = baggagemoment;
+
+  document.getElementById('zfm_leverarm').innerHTML = zfmleverarm;
+  document.getElementById('zfm_leverarm2').innerHTML = zfmleverarm;
+  document.getElementById('zfm_mass').innerHTML = zfmMass;
+  document.getElementById('zfm_moment').innerHTML = zfmMoment;
+  document.getElementById('fuel_mass').innerHTML = fuelmass;
+  document.getElementById('fuel_moment').innerHTML = fuelmoment;
+  document.getElementById('tom_leverarm').innerHTML = tomLeverarm;
+  document.getElementById('tom_leverarm2').innerHTML = tomLeverarm;
+  document.getElementById('tom_mass').innerHTML = tomMass;
+  document.getElementById('tom_moment').innerHTML = tomMoment;
+  document.getElementById('fuelburn_mass').innerHTML = fuelburnmass;
+  document.getElementById('fuelburn_moment').innerHTML = fuelburnmoment;
+  document.getElementById('lm_leverarm').innerHTML = lmLeverarm;
+  document.getElementById('lm_leverarm2').innerHTML = lmLeverarm;
+  document.getElementById('lm_mass').innerHTML = lmMass;
+  document.getElementById('lm_moment').innerHTML = lmMoment;
+
+}
+
   $("#numberbutton").click(function () {
     $("#numbersOutput").val('');
     var numbersResult = '';
@@ -52,4 +144,8 @@ function copyTheNumbers() {
 
      $('#numbersOutput').trigger('blur');
 
+}
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
 }
